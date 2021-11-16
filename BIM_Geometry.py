6df8716eb27292017cccf47e8ecf01cfe0e331a1
mjgordon <->
2021-11-16 17:08:34 +0100

    Added beam splitting and basic graph creation

---
+++ BIM_Geometry.py
@@ -0,0 +1,100 @@
+import numpy as np
+import open3d as o3d
+
+
+class Collision:
+    def __init__(self, a, b, precedence):
+        self.a = a
+        self.b = b
+        self.precedence = precedence
+
+    def equals(self, other):
+        return (self.a == other.a and self.b == other.b) or (self.a == other.b and self.b == other.a)
+
+
+class BeamSystemLayer():
+    def __init__(self):
+        self.beams = []
+        self.mean_spacing = -1
+
+    def add_beam(self,beam):
+        self.beams.append(beam)
+
+    def finalize(self):
+        distances = np.zeros(len(self.beams) - 1)
+        for i in range(len(self.beams) - 1):
+            beam_a = self.beams[i]
+            beam_b = self.beams[i + 1]
+            distances[i] = abs(beam_a.aabb.get_center()[beam_a.axis] - beam_b.aabb.get_center()[beam_b.axis])
+            self.mean_spacing = np.mean(distances)
+        print(self.mean_spacing)
+
+
+class Beam:
+
+    # Possibly stupid method to progressively assign ids to beams/nodes
+    beam_id_counter = 0
+
+    def __init__(self, aabb, axis, cloud):
+        self.aabb = aabb
+        self.axis = axis
+        self.long_axis = int(not axis)
+        self.cloud = cloud
+        self.length = aabb.get_extent()[int(not axis)]
+        self.id = Beam.beam_id_counter
+        Beam.beam_id_counter += 1
+
+    def check_overlap(self, other):
+        sc = self.aabb.get_center()
+        se = self.aabb.get_half_extent()
+        oc = other.aabb.get_center()
+        oe = other.aabb.get_half_extent()
+
+        x = abs(sc[0] - oc[0]) < (se[0] + oe[0])
+        y = abs(sc[1] - oc[1]) < (se[1] + oe[1])
+        z = abs(sc[2] - oc[2]) < (se[2] + oe[2])
+
+        # -1 Precedence implies a corner connection
+        precedence = -1
+
+        # Beams sitting on top of each other
+        if (sc[2] - oc[2]) > (se[2] + oe[2]) * 0.75:
+            precedence = self.axis
+
+        if (oc[2] - sc[2]) > (oe[2] + se[2]) * 0.75:
+            precedence = other.axis
+
+        # Beams touching
+        # Except first we need beam splitting
+
+        if x and y and z:
+            return Collision(self, other, precedence)
+        else:
+            return None
+
+    def get_point_param(self, point):
+        """Returns the world units position of a point projected to the beam"""
+        p_value = point[self.long_axis]
+        p_value -= (self.aabb.get_center()[self.long_axis] - self.aabb.get_half_extent()[self.long_axis])
+        return p_value
+
+    def split(self, position):
+        min_bound = self.aabb.get_min_bound()
+        max_bound = self.aabb.get_max_bound()
+
+        max_a = np.copy(max_bound)
+        max_a[self.long_axis] = position + min_bound[self.long_axis]
+
+        min_b = np.copy(min_bound)
+        min_b[self.long_axis] = position + min_bound[self.long_axis]
+
+        aabb_a = o3d.geometry.AxisAlignedBoundingBox(min_bound, max_a)
+        aabb_b = o3d.geometry.AxisAlignedBoundingBox(min_b, max_bound)
+
+        pc_a = self.cloud.crop(aabb_a)
+        pc_b = self.cloud.crop(aabb_b)
+
+        beam_a = Beam(aabb_a, self.axis, pc_a)
+        beam_b = Beam(aabb_b, self.axis, pc_b)
+
+        return beam_a, beam_b
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+import networkx as nx
 import numpy as np
 import open3d as o3d
 import os
@@ -9,8 +11,8 @@ import time
 from matplotlib import pyplot as plt
 from tkinter import filedialog
 
+from BIM_Geometry import Beam,BeamSystemLayer
 
-# Adjust lengths of major lines
 # Compare to walls
 # Make basic material judgement
 # Decide Splits
@@ -28,7 +30,12 @@ color_front_highlight = 'cyan'
 
 variance_split = 0.075
 
-bins = 256
+# Intended bin size in mm
+bin_width = 50
+
+beam_layers = []
+
+DG = nx.DiGraph()
 
 def set_up_vector(vis):
     vis.get_view_control().set_up((0.001, 0.001, 0.9999))
@@ -49,7 +56,11 @@ def get_slice(pc, aabb, axis, position, width, normalized=False):
 
     new_min[axis] = position - (width / 2)
     new_max[axis] = position + (width / 2)
-
+    #print(axis)
+    #print(position)
+    #print(width)
+    #print(new_min)
+    #print(new_max)
     bb = o3d.geometry.AxisAlignedBoundingBox(new_min, new_max)
     pc_slice = pc.crop(bb)
     return pc_slice
@@ -75,7 +86,7 @@ def normalize_histogram(hist):
 
 
 def process_histogram(hist):
-    hist_smooth = smooth_histogram(np.copy(hist))
+    hist_smooth = smooth_histogram(np.copy(hist),extension=2)
     hist_smooth = normalize_histogram(hist_smooth)
     hist = normalize_histogram(hist)
     return hist, hist_smooth
@@ -90,7 +101,9 @@ def get_peak_slice_params(hist, peak, diff=0.1):
     :return:
     """
     low = peak
-    high = peak
+    high = peak+1
+
+    #print("Peak : {}".format(peak))
 
     for i in range(low - 1,-1,-1):
         if hist[peak] - hist[i] < diff:
@@ -111,11 +124,13 @@ def get_peak_slice_params(hist, peak, diff=0.1):
 def analyze_z_levels(pc, aabb):
     points = np.asarray(pc.points)
 
-    hist_z, bin_edges = np.histogram(points[:, 2], bins)
+    z_bins = math.ceil(aabb.get_extent()[2] / bin_width)
+
+    hist_z, bin_edges = np.histogram(points[:, 2], z_bins)
     hist_z, hist_z_smooth = process_histogram(hist_z)
     mean_z = np.mean(hist_z_smooth)
 
-    peaks, properties = signal.find_peaks(hist_z_smooth, width=3, prominence=0.1)
+    peaks, properties = signal.find_peaks(hist_z_smooth, width=1, prominence=0.1)
 
     px = 1 / plt.rcParams['figure.dpi']  # pixel in inches
     fig, axs = plt.subplots(3, 3, figsize=(1920*px, 1080*px))
@@ -129,32 +144,40 @@ def analyze_z_levels(pc, aabb):
     plt.setp(axs[1, 0], ylabel='X Axis')
     plt.setp(axs[2, 0], ylabel='Y Axis')
 
+    #print("Peaks : {}".format(peaks))
+
     for i, peak in enumerate(peaks):
+
         # Highlight peaks in Z-plot
         bar_list_z_smooth[peak].set_color(color_back_highlight)
         bar_list_z[peak].set_color(color_front_highlight)
-
+        #continue
         # Get extents of peak
         peak_slice_position, peak_slice_width = get_peak_slice_params(hist_z_smooth, peak, 0.1)
 
         # Get slice at Z height
-        pc_slice = get_slice(pc, aabb, 2, peak_slice_position / bins, peak_slice_width / bins, normalized=True)
+        pc_slice = get_slice(pc, aabb, 2, peak_slice_position / z_bins, peak_slice_width / z_bins, normalized=True)
         pc_slice_aabb = pc_slice.get_axis_aligned_bounding_box()
         slice_points = np.asarray(pc_slice.points)
 
         rel_height = 0.75 # Check the width near the bottom of the peak
         prominence = 0.13 # Experimentally tuned, this should be determined more exactly
+        peak_width = 4
         # Take histogram along X axis
-        hist_x, bins_x = np.histogram(slice_points[:, 0], bins)
+        bin_count_x = math.ceil(pc_slice_aabb.get_extent()[0] / bin_width)
+        #print("Bin Count X : {}".format(bin_count_x))
+        hist_x, _ = np.histogram(slice_points[:, 0], bin_count_x)
         hist_x, hist_x_smooth = process_histogram(hist_x)
         mean_x = np.mean(hist_x_smooth)
-        peaks_x, _ = signal.find_peaks(hist_x_smooth, width=4, prominence=prominence, rel_height=rel_height)
+        peaks_x, _ = signal.find_peaks(hist_x_smooth, width=peak_width, prominence=prominence, rel_height=rel_height)
 
         # Take histogram along Y Axis
-        hist_y, bins_y = np.histogram(slice_points[:, 1], bins)
+        bin_count_y = math.ceil(pc_slice_aabb.get_extent()[1] / bin_width)
+        #print("Bin Count Y : {}".format(bin_count_y))
+        hist_y, _ = np.histogram(slice_points[:, 1], bin_count_y)
         hist_y, hist_y_smooth = process_histogram(hist_y)
         mean_y = np.mean(hist_y_smooth)
-        peaks_y, _ = signal.find_peaks(hist_y_smooth, width=4, prominence=prominence, rel_height=rel_height)
+        peaks_y, _ = signal.find_peaks(hist_y_smooth, width=peak_width, prominence=prominence, rel_height=rel_height)
 
         # Plot X axis histogram and mean
         bar_list_x_smooth = axs[1, i].bar(range(len(hist_x_smooth)), hist_x_smooth, color=color_back, width=1)
@@ -178,47 +201,63 @@ def analyze_z_levels(pc, aabb):
         variance_x = np.var(hist_x)
         variance_y = np.var(hist_y)
 
+        print("Peak : {}, Variance X : {}, Variance Y : {}".format(peak,variance_x,variance_y))
+
         if variance_x < variance_split or variance_y < variance_split:
             o3d.io.write_point_cloud(dir_output + filename + "_grid_{}.ply".format(peak), pc_slice)
-            analyze_beam_system(pc_slice, pc_slice_aabb, 0, hist_x_smooth, peaks_x)
-            analyze_beam_system(pc_slice, pc_slice_aabb, 1, hist_y_smooth, peaks_y)
-
-    plt.savefig(dir_output + filename + "_plot.png")
-    plt.show()
+            analyze_beam_system(pc_slice, pc_slice_aabb, 0, hist_x_smooth, peaks_x,bin_count_x)
+            analyze_beam_system(pc_slice, pc_slice_aabb, 1, hist_y_smooth, peaks_y,bin_count_y)
 
 
-def analyze_beam_system(pc, aabb, axis, hist, peaks):
-    global dumb_flag
 
+
+def analyze_beam_system(pc, aabb, axis, hist, peaks, source_bin_count):
+    not_axis = int(not axis)
+    beam_layers.append(BeamSystemLayer())
     for peak in peaks:
         slice_position, slice_width = get_peak_slice_params(hist, peak, 0.1)
-        beam_slice = get_slice(pc, aabb, axis, slice_position / bins, slice_width / bins, normalized=True)
+        #print("beam!")
+        #print(slice_position)
+        #print(slice_width)
+        beam_slice = get_slice(pc, aabb, axis, slice_position / source_bin_count, slice_width / source_bin_count, normalized=True)
         beam_slice_points = np.array(beam_slice.points)
         beam_aabb = beam_slice.get_axis_aligned_bounding_box()
+        extent = beam_aabb.get_extent()
 
-        beam_hist, _ = np.histogram(beam_slice_points[:, int(not axis)], bins)
+        bin_count = math.ceil(beam_aabb.get_extent()[not_axis] / bin_width)
+        #print("Bin Count : {}".format(bin_count))
+
+        beam_hist, _ = np.histogram(beam_slice_points[:, not_axis], bin_count)
         beam_hist = smooth_histogram(beam_hist, 1)
 
-        low = bins // 2
-        high = bins // 2
+        # Count out from the median value
+        median = np.median(beam_slice_points[:, not_axis])
+        median_bin = int((median - (beam_aabb.get_center()[not_axis] - beam_aabb.get_half_extent()[not_axis])) / beam_aabb.get_extent()[not_axis] * bin_count)
+        #print("Median {} in {} to {}".format(median,beam_aabb.get_center()[not_axis] - beam_aabb.get_half_extent()[not_axis],beam_aabb.get_center()[not_axis] + beam_aabb.get_half_extent()[not_axis]))
+        #print("Median bin : " + str(median_bin))
+        low = median_bin
+        high = median_bin + 1
         for i in range(low, -1, -1):
             if beam_hist[i] > 0:
                 low = i
             else:
                 break
-        for i in range(high, bins):
+        for i in range(high, bin_count):
             if beam_hist[i] > 0:
                 high = i
             else:
                 break
         slice_width = high - low
         slice_position = slice_width / 2 + low
-        print(slice_position / bins)
-        print(slice_width / bins)
-        beam_slice = get_slice(beam_slice, beam_aabb, int(not axis), slice_position / bins, slice_width / bins, normalized=True)
+
+        beam_slice = get_slice(beam_slice, beam_aabb, int(not axis), slice_position / bin_count, slice_width / bin_count, normalized=True)
         beam_aabb = beam_slice.get_axis_aligned_bounding_box()
-        beam_aabb.color = (0,1,1) if axis else (1,0,1)
+        beam_aabb.color = (0, 1, 1) if axis else (1, 0, 1)
+
+        beam_layers[-1].add_beam(Beam(beam_aabb, axis, beam_slice))
+        #vis.add_geometry(beam_slice)
         vis.add_geometry(beam_aabb)
+    beam_layers[-1].finalize()
 
 
 def setup_vis():
@@ -229,6 +268,44 @@ def setup_vis():
     vis.get_render_option().point_size = 1
 
     return vis
+
+
+def perform_beam_splits(primary_layer, secondary_layer):
+    # Perhaps we need to check for actual intersection first but for now theres no issues
+    new_secondary = BeamSystemLayer()
+    for sb in secondary_layer.beams:
+        for pb in primary_layer.beams:
+            location = sb.get_point_param(pb.aabb.get_center())
+            if 0 < location < sb.length:
+                if 0.1 < location < (sb.length - 0.1):
+                    new_a, new_b = sb.split(location)
+                    new_secondary.add_beam(new_a)
+                    new_secondary.add_beam(new_b)
+                    if sb in new_secondary.beams:
+                        print("deleted")
+                        new_secondary.beams.remove(sb)
+                else:
+                    new_secondary.add_beam(sb)
+    return new_secondary
+
+
+
+
+def analyze_beam_connections(primary_layer,secondary_layer):
+    # Create edges
+    for sb in secondary_layer.beams:
+        for pb in primary_layer.beams:
+            if sb.check_overlap(pb):
+                DG.add_edges_from([(sb.id, pb.id)])
+
+    # Set node_layers
+    for pb in primary_layer.beams:
+        if pb.id in DG.nodes:
+            DG.nodes[pb.id]['layer'] = 0
+    for sb in secondary_layer.beams:
+        if sb.id in DG.nodes:
+            DG.nodes[sb.id]['layer'] = 1
+
 
 
 # === MAIN SCRIPT ===
@@ -251,6 +328,7 @@ if __name__ == '__main__':
     vis = setup_vis()
 
     cloud = o3d.io.read_point_cloud(filepath)
+    print(cloud)
     vis.add_geometry(cloud)
 
     aabb_main = cloud.get_axis_aligned_bounding_box()
@@ -261,6 +339,27 @@ if __name__ == '__main__':
     vis.add_geometry(mesh_frame)
 
     analyze_z_levels(cloud, aabb_main)
+    plt.savefig(dir_output + filename + "_plot.png")
+    plt.show()
+
+    if len(beam_layers) > 2:
+        print("Error : Handling more than 2 beam layers not yet implemented")
+    primary_id = int(beam_layers[0].mean_spacing < beam_layers[1].mean_spacing)
+    secondary = perform_beam_splits(beam_layers[primary_id], beam_layers[int(not primary_id)])
+
+    for beam in secondary.beams:
+        vis.add_geometry(beam.cloud)
+        vis.add_geometry(beam.aabb)
+
+    analyze_beam_connections(beam_layers[primary_id],secondary)
+
+    pos = nx.multipartite_layout(DG, 'layer')
+    nx.draw(DG, pos, with_labels=True)
+
+    plt.show()
+
+
+    vis.remove_geometry(cloud)
 
     vis.run()
     vis.destroy_window()
